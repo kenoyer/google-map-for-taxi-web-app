d4ea1800421130aa685fb7f0ae459cc2c3338012
Nandha <nandha21.2@gmail.com>
2014-09-09 17:14:07 +0530

    layout finished

--- assets/script.js
+++ assets/script.js
@@ -6,5 +6,13 @@ function initialize() {
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
- }
+		
+		google.maps.event.addListener(map, 'click', function(event) {
+    		var marker = new google.maps.Marker({
+    			position: event.latLng,
+    			map: map,
+    			title:"Hello World!"
+			});
+  		});
+}
  google.maps.event.addDomListener(window, 'load', initialize);
